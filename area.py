451df843c43463957969fcc6ecf9ba1d4b943e9b
mitoaustine <123359457+mitoaustine@users.noreply.github.com>
2023-01-23 12:21:33 +0300

    Add files via upload
    Lesson 2

---
+++ area.py
@@ -0,0 +1,12 @@
+# Program to find the area of a circle
+""""
+ Requirements => pie=3.14, radius
+ formula of area => pie *radius*radius
+"""
+
+pie = 3.14
+radius = int(input("Enter radius: "))
+
+area = pie*radius*radius
+
+print(area)
---
+++ work.py
@@ -0,0 +1,7 @@
+# A program that takes in the hours worked and the rate of work and computes the gross pay
+
+hours_worked = int(input("Hours worked: "))
+rate_per_hour =2.75
+
+gross_pay = hours_worked*rate_per_hour
+print(gross_pay)
