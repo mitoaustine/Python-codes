9b37f5a620e7a87cc5720262ab3eccf2e6374d08
mitoaustine <123359457+mitoaustine@users.noreply.github.com>
2023-02-24 09:18:57 +0300

    Add files via upload

---
+++ mito.py
@@ -0,0 +1,9 @@
+ 
+def area_of_the_circle (Radius): 
+    pie=3.14
+    area = Radius** 2 * pie  
+    return area  
+Radius = float (input ("Please enter the radius of the given circle: "))  
+print (" The area of the given circle is: ", area_of_the_circle (Radius))  
+    
+     
