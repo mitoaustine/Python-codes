b3b1156c8d6c306aa719d1d0bd4f88d1d91e91a2
mitoaustine <123359457+mitoaustine@users.noreply.github.com>
2023-03-03 09:51:38 +0300

    Add files via upload

---
+++ creatingclassesnobjects.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self,name,percentage):
+        self.name=name
+        self.percentage=percentage
+    def show(self):
+        print("Name is:",self.name,"and percentage is:",self.percentage)
+stud=Student("Mito",90)
+stud.show()
+    
