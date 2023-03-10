f2353658f455487a17085c5157d5a5629c197907
mitoaustine <123359457+mitoaustine@users.noreply.github.com>
2023-03-10 09:01:23 +0300

    Add files via upload

---
+++ simpleinheritance.py
@@ -0,0 +1,12 @@
+class  Animal:
+    def legs(self):
+        print("I have four legs")
+    def fur(self):
+        print("My body is covered with fur")
+class Dog(Animal):
+    def bark(self):
+        print("The dog barks")
+d=Dog()
+d.bark()
+d.legs()
+d.fur()
